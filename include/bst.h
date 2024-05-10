4356ed0bc69c158b39c7cd2a5643027a29d136e5
Xing Zhikai <haoyunbaobei1209@gmail.com>
2024-05-10 18:15:46 +0800

    Update ans
    pass all unit test and no memory leak

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,56 @@
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <vector>
 #ifndef BST_H
 #define BST_H
+class BST {
+ public:
+  class Node {
+   public:
+    Node(int value, Node *left, Node *right);
+    Node();
+    Node(const Node &node);
 
+    int value;
+    Node *left;
+    Node *right;
 
-#endif //BST_H
+    friend std::ostream &operator<<(std::ostream &os, const Node &node);
+    bool operator==(const int &val) const;
+    bool operator!=(const int &val) const;
+    bool operator<(const int &val) const;
+    bool operator>=(const int &val) const;
+    bool operator>(const int &val) const;
+    bool operator<=(const int &val) const;
+  };
+  Node *&get_root();
+  void bfs(std::function<void(Node *&node)> func);
+  size_t length();
+  bool add_node(int value);
+  Node **find_node(int value);
+  Node **find_parrent(int value);
+  Node **find_successor(int value);
+  bool delete_node(int value);
+  friend std::ostream &operator<<(std::ostream &os, BST &bst);
+  BST();
+  BST(BST &other);
+  BST(BST &&other);
+  // BST(const std::vector<int> &arr);
+  BST(const std::initializer_list<int> &list);
+  BST &operator=(BST &other);
+  BST &operator=(BST &&other);
+  ~BST();
+  BST &operator++();    // pre-increment
+  BST operator++(int);  // post-increment
+
+ private:
+  Node *root;
+};
+bool operator==(const int &val, const BST::Node &node);
+bool operator!=(const int &val, const BST::Node &node);
+bool operator<(const int &val, const BST::Node &node);
+bool operator>=(const int &val, const BST::Node &node);
+bool operator>(const int &val, const BST::Node &node);
+bool operator<=(const int &val, const BST::Node &node);
+#endif  // BST_H
